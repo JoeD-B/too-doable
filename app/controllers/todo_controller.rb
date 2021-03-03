77bd0baa6120f5d285e1e5394ce37852771e2d5e
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-03-03 11:48:03 -0500

    added edit and delete. edit is slightly broken: makes the update at the todo/:id page but not at the index

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -13,6 +13,27 @@ class TodoController < ApplicationController
         @todo = Todo.create(name: params[:name])
         erb :'todo/show'
     end
-    
-    
+    get '/todo/:id' do
+        @todo = Todo.find_by(id: params[:id])
+        
+        erb :'todo/show'
+    end
+
+    get '/todo/:id/edit' do
+        @todo = Todo.find_by(id: params[:id])
+        erb :'todo/edit'
+    end
+    patch '/todo/:id' do
+        
+        todo = Todo.find_by(id: params[:id])
+        todo.update(name: params[:name])
+        erb :'todo/show'
+    end
+    delete '/todo/:id' do
+        todo = Todo.find_by(id: params[:id])
+        todo.delete
+        # @todos = todo.all
+        # erb :'todos/index'
+        redirect('/todo')
+      end
 end
